62396dfe3089930ca5cd4d88d4c0eccc6a9856a2
ruben hernan pared <hernan2014@hotmail.com>
2024-08-01 11:47:39 -0300

    coneccion con base de datos novedades

--- models/novedadesModel.js
+++ models/novedadesModel.js
@@ -1,15 +1,9 @@
-var pool = require('./bd');
+var pool = require("./bd");
 
 async function getNovedades() {
-    var query = "SELECT * FROM novedades ORDER BY id DESC";
-    var rows = await pool.query(query);
-    return rows;
+  var query = "SELECT * FROM novedades ORDER BY id ASC";
+  var rows = await pool.query(query);
+  return rows;
 }
 
-async function addNovedad(titulo, subtitulo, cuerpo) {
-    var query = "INSERT INTO novedades (titulo, subtitulo, cuerpo) VALUES (?, ?, ?)";
-    var rows = await pool.query(query, [titulo, subtitulo, cuerpo]);
-    return rows;
-}
-
-module.exports = { getNovedades, addNovedad };
+module.exports = { getNovedades };
--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,55 +1,21 @@
-var express = require('express');
+var express= require('express');
 var router = express.Router();
-var novedadesModel = require('../../models/novedadesModel');
-
-router.get('/', function (req, res, next) {
-    res.render('admin/novedades', {
-        layout: 'admin/layout',
-        usuario: req.session.nombre,
-    });
-});
+var novedadesModel = require('../../models/novedadesModel')
 
 
-// Listar Novedades
-router.get('/', async function(req, res, next) {
+//para listar novedades
+
+router.get('/', async function(req, res, next){
+
     var novedades = await novedadesModel.getNovedades();
+
     res.render('admin/novedades', {
         layout: 'admin/layout',
         usuario: req.session.nombre,
         novedades
     });
+
 });
 
-// Mostrar formulario de alta
-router.get('/agregar', function(req, res, next) {
-    res.render('admin/agregar', {
-        layout: 'admin/layout'
-    });
-});
-
-// Agregar Novedad
-router.post('/agregar', async function(req, res, next) {
-    try {
-        var { titulo, subtitulo, cuerpo } = req.body;
-
-        if (titulo && subtitulo && cuerpo) {
-            await novedadesModel.addNovedad(titulo, subtitulo, cuerpo);
-            res.redirect('/admin/novedades');
-        } else {
-            res.render('admin/agregar', {
-                layout: 'admin/layout',
-                error: true,
-                message: 'Todos los campos son requeridos'
-            });
-        }
-    } catch (error) {
-        console.log(error);
-        res.render('admin/agregar', {
-            layout: 'admin/layout',
-            error: true,
-            message: 'Error al agregar la novedad'
-        });
-    }
-});
 
 module.exports = router;
